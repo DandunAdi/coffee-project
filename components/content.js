c3ad6034a3fc0894e286fbd9bea778e7803d52cf
Dandun Adi <dandun.adi@gmail.com>
2021-05-27 17:19:34 +0700

    update content

--- components/content.js
+++ components/content.js
@@ -8,15 +8,14 @@ const Content = () => {
         <div className={styles.contentBg}>
           <Image src="/images/coffee_art.png" width="271" height="503" />
         </div>
-        <h3 className="xl">WHERE DOES OUR COFFEE COME FROM?</h3>
+        <h3 className="xl">KOPI INDONESIA DAN KOPI LOKAL BANJARNEGARA</h3>
         <p className={styles.contentBody}>
-          We import good beans, roast them with care and brew them to the best
-          of our abilities for the enjoyment of ourselves and all people who
-          appreciate great coffee.
+          Kami menyediakan kopi asli Indonesia mulai dari Kerinci, Bali
+          Kintamani, Gayo, Temanggung, Toraja Kalosi s.d Padang Solok. Tersedia
+          dalam bentuk green bean, roasted bean, dan bubuk. Tersedia juga kopi
+          lokal Banjarnegara mulai dari Babadan, Kasmaran, Leksana, Ratamba,
+          Senggani dan Wanayasa.
         </p>
-        <div className="flex" style={{ marginTop: "3rem", textShadow: "none" }}>
-          <span className="btn sm">READ MORE</span>
-        </div>
       </div>
     </section>
   );
